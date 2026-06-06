8d7869116b261fe089bf75403ba6bb3bf6e6fc95
timmyades3 <oluwatimileyinadesina623@gmail.com>
2026-06-06 08:07:02 +0100

    refactor: changed shedule time in celery.py

--- core/celery.py
+++ core/celery.py
@@ -1,6 +1,6 @@
 import os
 from celery import Celery
-from celery.schedules import crontab
+from datetime import timedelta
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
@@ -11,15 +11,14 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
-
-	'hourly-aggregate-and-cleanup': {
-		'task': 'monitoring.tasks.hourly_aggregate_and_cleanup',
-		'schedule': crontab(minute='*/6'),
-	},
-	'daily-aggregate-and-cleanup': {
-		'task': 'monitoring.tasks.daily_aggregate_and_cleanup',
-		'schedule': crontab(minute='*/18'),
-	}
+    'hourly-aggregate-and-cleanup': {
+        'task': 'monitoring.tasks.hourly_aggregate_and_cleanup',
+        'schedule': timedelta(hours=1),
+    },
+    'daily-aggregate-and-cleanup': {
+        'task': 'monitoring.tasks.daily_aggregate_and_cleanup',
+        'schedule': timedelta(hours=24),
+    },
 }
 
 
